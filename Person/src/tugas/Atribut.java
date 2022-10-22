cdd4b88cdf1ce842ab76a6ab087f981ba6c49184
adityabudi09 <115923969+adityabudi09@users.noreply.github.com>
2022-10-22 07:25:14 +0700

    Add files via upload

---
+++ Person/src/tugas/Atribut.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author DWI ADITYA BUDI
+ */
+public class Atribut {
+    public static void main(String[] args){
+        
+        Person orang = new Person();
+        
+        orang.nama1="Riko";
+        orang.nama2="cika";
+        orang.jeniskelamin1="laki-laki";
+        orang.jeniskelamin2="perempuan";
+        orang.umur1=19;
+        orang.umur2=20;
+        
+        orang.Person();
+        
+    }
+    
+}
